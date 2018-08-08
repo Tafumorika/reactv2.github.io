6dd2ca6740cccaa93b5532d91c53d2cdb8bb9103
Tafumorika <tafumorika@mail.ru>
2018-08-08 17:16:42 +0700

    PropTypes добавлен в компоненты

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
 const Link = (props) => {
-    const {href} = props;
+    const {href, children} = props;
     return(
-        <a href={href} className="color-text">{props.children}</a>
+        <a href={href} className="color-text">{children}</a>
     )
-}
+};
+
+Link.propTypes = {
+    href: PropTypes.string,
+    children: PropTypes.node
+};
 export {Link}
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
 
@@ -10,5 +11,10 @@ const Main = (props) => {
             </main>
         </div>
     )
-}
+};
+
+Main.propTypes ={
+    children: PropTypes.node
+};
+
 export {Main}
